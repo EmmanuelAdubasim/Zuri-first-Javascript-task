e2b747b349cdf5b7a7d311d3c3066ccc3246e23d
EmmanuelAdubasim <adubasimemmanuelikechukwu01@gmail.com>
2022-06-20 19:44:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let fName = 'Emmanuel';
+let nName = 'Adubasim';
+let urHeight = "180cm";
+let urCountry = 'Nigeria';
+
+console.log(fName, nName +' is my name');
+console.log(urHeight +' is my estimated height');
+console.log(urCountry +' is my Country');
